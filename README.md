12c1a9bdfe2b481e63043133b3af4f211fb12b6b
mmoghal <mmoghal29@gmail.com>
2023-04-02 06:32:10 -0400

    Updated README file. Commented html, css and js code. Added image Performed final code review

--- README.md
+++ README.md
@@ -1,38 +1,53 @@
-# weather-dashboard
-weather-dashboard
+# Weather-Dashboard
+
+## Description
+
 This is a weather dashboard that allows users to search for the weather outlook of multiple cities. Users can search for a city and view the current and future weather conditions for that city. The city is added to the search history for easy access. Users can also click on a city in the search history to view the current and future conditions for that city.
 
-Technologies Used
-HTML
-CSS
-JavaScript
-OpenWeatherMap API
-Installation
+## Technologies Used
+
+. HTML
+
+. CSS
+
+. JavaScript
+
+. OpenWeatherMap API
+
+## Installation
+
 To install and run this application, follow these steps:
 
-Clone the repository to your local machine.
-Open the index.html file in your web browser.
-Enter a city name or zip code in the search bar and click the "Search" button.
-The current weather and 5-day forecast for the city will be displayed.
-Usage
+. Clone the repository to your local machine.
+
+. Open the index.html file in your web browser.
+
+. Enter a city name or zip code in the search bar and click the "Search" button.
+
+. The current weather and 5-day forecast for the city will be displayed.
+
+## Usage
+
 To use this application, simply enter a city name or zip code in the search bar and click the "Search" button. The current weather and 5-day forecast for the city will be displayed. The search history is also displayed for easy access to previously searched cities.
 
-Credits
-This application uses the OpenWeatherMap API to retrieve weather data.
+## Credits
 
-License
+This weather-dashboard application was built by Muhammad Moghal using the OpenWeatherMap API to retrieve weather data.
+
+## License
+
 This project is licensed under the MIT License.
 
-Screenshots
-(Optional) You can include screenshots of your application in action to help users better understand how to use it.
+## Screenshots
 
-Screenshot of Weather Dashboard
+1.  Visit [Deployed Application](https://mmoghal.github.io/weather-dashboard/) for weather-dashboard
 
-Contributing
+2.  Screenshot of Weather Dashboard ![alt Image of the application](https://github.com/mmoghal/weather-dashboard/blob/main/assets/images/weather.png)
+
+## Contributing
+
 If you would like to contribute to this project, please fork the repository and submit a pull request.
 
-Contact
-If you have any questions or issues, please contact me at .
+## Contact
 
-
-
+If you have any questions or issues, please contact me at mmoghal29@gmail.com
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,14 @@
 // Get element references
-const searchInput = document.getElementById('search');
-const form = document.getElementById('form');
-const currentTemp = document.getElementById('temp');
-const currentDesc = document.getElementById('desc');
-const currentHumidity = document.getElementById('humidity');
-const currentWind = document.getElementById('wind');
-const forecastEl = document.getElementById('forecast-table');
+const searchInput = document.getElementById("search");
+const form = document.getElementById("form");
+const currentTemp = document.getElementById("temp");
+const currentDesc = document.getElementById("desc");
+const currentHumidity = document.getElementById("humidity");
+const currentWind = document.getElementById("wind");
+const forecastEl = document.getElementById("forecast-table");
 
-// Define event listeners
-form.addEventListener('submit', (e) => {
+// Define event listeners for when the form is submitted
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchTerm = searchInput.value;
   if (isValidZip(searchTerm)) {
@@ -19,102 +19,31 @@ form.addEventListener('submit', (e) => {
     addToSearchHistory(searchTerm);
   }
   displayDateTime();
-
 });
 
-function displayDateTime() {
-  const dateTime = new Date();
-  const dateTimeEl = document.getElementById('date-time');
-  dateTimeEl.textContent = `Current date and time: ${dateTime}`;
-}
-
-
-
-
-
-// Function to add the search term to the search history
-function addToSearchHistory(searchTerm) {
-  // Get the search history array from local storage or create a new one if it doesn't exist
-  let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-
-  // Add the search term to the search history array
-  searchHistory.unshift(searchTerm);
-
-  // Keep only the last 10 search terms in the search history array
-  searchHistory = searchHistory.slice(0, 10);
-
-  // Save the updated search history array to local storage
-  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-
-  // Call the function to display the search history
-  displaySearchHistory();
-}
-
-// Function to display the search history
-function displaySearchHistory() {
-  // Get the search history array from local storage or create a new one if it doesn't exist
-  let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-
-  // Get the search history element from the DOM
-  const searchHistoryEl = document.getElementById('search-history');
-
-  // Clear the previous search history
-  searchHistoryEl.innerHTML = '';
-
-  // Loop through each search term in the search history and display it on the page
-  searchHistory.forEach(searchTerm => {
-    const searchItemEl = document.createElement('div');
-    searchItemEl.textContent = searchTerm;
-    searchHistoryEl.appendChild(searchItemEl);
-  });
-}
-
-
-
-
-// Functions
+// Functions to checks if the given zip code is valid or not
 function isValidZip(zip) {
   return /^\d{5}(-\d{4})?$/.test(zip);
 }
 
+// Function for current date and time
+function displayDateTime() {
+  const dateTime = new Date();
+  const dateTimeEl = document.getElementById("date-time");
+  dateTimeEl.textContent = `Current date and time: ${dateTime}`;
+}
+
+// Function to define two API endpoint paths for retrieving weather data by zip code
 function getWeatherByZip(zip) {
-  const apiKey = '81822968b5a226abb1a2fbacd053f10a';
-  const units = 'imperial';
+  const apiKey = "81822968b5a226abb1a2fbacd053f10a";
+  const units = "imperial";
   const currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
   const forecastPath = `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${apiKey}&units=${units}`;
 
   // Get current weather
   fetch(currentWeatherPath)
-    .then(res => res.json())
-    .then(json => {
-      currentTemp.innerHTML = `${json.main.temp} &deg;F`;
-      currentDesc.innerHTML = json.weather[0].description;
-      currentHumidity.innerHTML = `Humidity: ${json.main.humidity}%`;
-      currentWind.innerHTML = `Wind: ${json.wind.speed} mph`;
-
-// Get weather icon
-const iconCode = json.weather[0].icon;
-const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-const iconEl = document.createElement('img');
-iconEl.src = iconUrl;
-currentDesc.prepend(iconEl);
-
-      // Call function to get 5-day forecast
-      getForecast(json.name);
-    })
-    .catch(err => console.log(err.message));
-}
-
-function getWeatherByCity(city) {
-  const apiKey = '81822968b5a226abb1a2fbacd053f10a';
-  const units = 'imperial';
-  const currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  const forecastPath = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
-
-  // Get current weather
-  fetch(currentWeatherPath)
-    .then(res => res.json())
-    .then(json => {
+    .then((res) => res.json())
+    .then((json) => {
       currentTemp.innerHTML = `${json.main.temp} &deg;F`;
       currentDesc.innerHTML = json.weather[0].description;
       currentHumidity.innerHTML = `Humidity: ${json.main.humidity}%`;
@@ -123,63 +52,128 @@ function getWeatherByCity(city) {
       // Get weather icon
       const iconCode = json.weather[0].icon;
       const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-      const iconEl = document.createElement('img');
+      const iconEl = document.createElement("img");
       iconEl.src = iconUrl;
       currentDesc.prepend(iconEl);
-
 
       // Call function to get 5-day forecast
       getForecast(json.name);
     })
-    .catch(err => console.log(err.message));
+    .catch((err) => console.log(err.message));
+}
+// Function to define two API endpoint paths for retrieving weather data by city name
+function getWeatherByCity(city) {
+  const apiKey = "81822968b5a226abb1a2fbacd053f10a";
+  const units = "imperial";
+  const currentWeatherPath = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  const forecastPath = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+
+  // Get current weather
+  fetch(currentWeatherPath)
+    .then((res) => res.json())
+    .then((json) => {
+      currentTemp.innerHTML = `${json.main.temp} &deg;F`;
+      currentDesc.innerHTML = json.weather[0].description;
+      currentHumidity.innerHTML = `Humidity: ${json.main.humidity}%`;
+      currentWind.innerHTML = `Wind: ${json.wind.speed} mph`;
+
+      // Get weather icon
+      const iconCode = json.weather[0].icon;
+      const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
+      const iconEl = document.createElement("img");
+      iconEl.src = iconUrl;
+      currentDesc.prepend(iconEl);
+
+      // Call function to get 5-day forecast
+      getForecast(json.name);
+    })
+    .catch((err) => console.log(err.message));
 }
 
 // Get 5-day forecast
-
 function getForecast(city) {
-  const apiKey = '81822968b5a226abb1a2fbacd053f10a';
-  const units = 'imperial';
+  const apiKey = "81822968b5a226abb1a2fbacd053f10a";
+  const units = "imperial";
   const path = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
   fetch(path)
-    .then(res => res.json())
-    .then(json => {
+    .then((res) => res.json())
+    .then((json) => {
       // Get the forecast data for each day
-      const forecasts = json.list.filter(item => item.dt_txt.includes('12:00:00'));
-    
+      const forecasts = json.list.filter((item) =>
+        item.dt_txt.includes("12:00:00")
+      );
+
       // Clear previous forecast data
-      forecastEl.innerHTML = '';
+      forecastEl.innerHTML = "";
 
       // Loop through each forecast and display it on the page
-      forecasts.forEach(forecast => {
-        const date = new Date(forecast.dt * 1000).toLocaleDateString('en-US', {
-          weekday: 'short'
+      forecasts.forEach((forecast) => {
+        const date = new Date(forecast.dt * 1000).toLocaleDateString("en-US", {
+          weekday: "short",
         });
         const temp = forecast.main.temp;
         const desc = forecast.weather[0].description;
         const iconCode = forecast.weather[0].icon;
 
-        const forecastItemEl = document.createElement('div');
-        forecastItemEl.classList.add('forecast-item');
+        const forecastItemEl = document.createElement("div");
+        forecastItemEl.classList.add("forecast-item");
 
-        const dateEl = document.createElement('p');
+        const dateEl = document.createElement("p");
         dateEl.textContent = date;
         forecastItemEl.appendChild(dateEl);
 
-        const tempEl = document.createElement('p');
+        const tempEl = document.createElement("p");
         tempEl.textContent = `Temperature: ${temp}°F`;
         forecastItemEl.appendChild(tempEl);
 
-        const descEl = document.createElement('p');
+        const descEl = document.createElement("p");
         descEl.textContent = `Description: ${desc}`;
         forecastItemEl.appendChild(descEl);
 
-        const iconEl = document.createElement('img');
-        iconEl.classList.add('forecast-icon');
+        const iconEl = document.createElement("img");
+        iconEl.classList.add("forecast-icon");
         iconEl.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
         forecastItemEl.appendChild(iconEl);
 
         forecastEl.appendChild(forecastItemEl);
       });
     })
-    .catch(err => console.log(err.message));
+    .catch((err) => console.log(err.message));
+}
+
+// Function to add the search term to the search history
+function addToSearchHistory(searchTerm) {
+  // Get the search history array from local storage or create a new one if it doesn't exist
+  let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+  // Add the search term to the search history array
+  searchHistory.unshift(searchTerm);
+
+  // Keep only the last 10 search terms in the search history array
+  searchHistory = searchHistory.slice(0, 10);
+
+  // Save the updated search history array to local storage
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+  // Call the function to display the search history
+  displaySearchHistory();
+}
+
+// Function to display the search history
+function displaySearchHistory() {
+  // Get the search history array from local storage or create a new one if it doesn't exist
+  let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+  // Get the search history element from the DOM
+  const searchHistoryEl = document.getElementById("search-history");
+
+  // Clear the previous search history
+  searchHistoryEl.innerHTML = "";
+
+  // Loop through each search term in the search history and display it on the page
+  searchHistory.forEach((searchTerm) => {
+    const searchItemEl = document.createElement("div");
+    searchItemEl.textContent = searchTerm;
+    searchHistoryEl.appendChild(searchItemEl);
+  });
 }
